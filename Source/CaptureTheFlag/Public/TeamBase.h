3099c781c085b7f154d309c2790be56c87f165ca
AVELL-LEO\leohi <leohirano95@gmail.com>
2025-04-28 06:13:00 -0300

    Fix Spawn Points not editable

--- Source/CaptureTheFlag/Public/TeamBase.h
+++ Source/CaptureTheFlag/Public/TeamBase.h
@@ -32,14 +32,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UBoxComponent* Box;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FTransform> SpawnPoints;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	FGameplayTag TeamTag;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TArray<FTransform> SpawnPoints;
 private:
 	UFUNCTION()
 	void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);	
